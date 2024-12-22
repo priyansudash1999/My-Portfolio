6bd99653dd3a224bb12d3a95b7fea75b4c59c088
Priyansu <1999priyansudash@gmail.com>
2024-12-22 13:32:27 +0530

    Design my project section

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -28,7 +28,7 @@ const Ball = (props) => {
   )
 }
 
-const BallCanvas = ({ icon }) => {
+const BallCanvas = ({ icon, title }) => {
   return (
     <Canvas
       frameloop='demand'
@@ -37,7 +37,7 @@ const BallCanvas = ({ icon }) => {
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
-        <Ball imgUrl={icon} />
+        <Ball imgUrl={icon}/>
       </Suspense>
 
       <Preload all />
